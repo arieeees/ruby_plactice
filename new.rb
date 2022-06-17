4e5cb80feb4917c001d30476a564a03a7efef2af
arieeees <105973830+arieeees@users.noreply.github.com>
2022-06-17 13:07:42 +0000

    [add]ruby_plactice

---
+++ new.rb
@@ -0,0 +1,23 @@
+class Car
+    def turn(a)
+        puts "#{a}に曲がります。"
+    end
+    
+    def run(distance)
+        puts "車で#{distance}キロ走ります。"
+    end
+end
+
+car = Car.new
+car.turn("右")
+
+car = Car.new
+car.run(5)
+
+class Car
+    def self.run(distance)
+        puts "車で#{distance}キロ走ります。"
+    end
+end
+
+Car.run(10)
